260da89ddcffeeb895fd19cb6602575dd5cfe02d
ALEXANDRE <lucasalexandre120921@gmail.com>
2025-10-21 15:04:30 -0300

    Criar um usuário de banco de dados com as permissões necessárias.
    criei 3 usuarios

--- concescionaria.sql
+++ concescionaria.sql
@@ -1,1 +1,4 @@
 CREATE DATABASE consessionaria_BD2;
+CREATE USER 'cliente'@'%' IDENTIFIED BY 'cli123';
+CREATE USER 'funcionario'@'%'IDENTIFIED BY 'func123';
+CREATE USER 'adm'@'%' IDENTIFIED BY 'adm123';
