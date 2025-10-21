382d0dffea118f83f8901ad0bfe5728abcb98411
ALEXANDRE <lucasalexandre120921@gmail.com>
2025-10-21 15:09:18 -0300

    Delete concescionaria.sql

--- concescionaria.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE consessionaria_BD2;
-CREATE USER 'cliente'@'%' IDENTIFIED BY 'cli123';
-CREATE USER 'funcionario'@'%'IDENTIFIED BY 'func123';
-CREATE USER 'adm'@'%' IDENTIFIED BY 'adm123';
