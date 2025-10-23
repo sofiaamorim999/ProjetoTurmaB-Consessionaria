47ee104bf83d19a67d4005660ec7589f2e72987c
ssaantos08 <luiznaovouesquecer@gmail.com>
2025-10-23 18:13:18 -0300

    Add tabelas
    Adicionei a tabela propostas e usuario a pasta Model.

---
+++ src/Model/Propostas.php
@@ -0,0 +1,15 @@
+<?
+
+class Propostas
+
+{
+    public int $id;
+    public int $veiculo_id;
+    public string $nome_cliente;
+    public string $email_cliente;
+    public string $telefone_cliente;
+    public string $mensagem;
+    public string $data_proposta;
+}
+
+
