d43f51f3d98b8570c976e786ef715b2bf0068248
PauloHenriqueDev26 <paulo.benedicto3679@gmail.com>
2025-11-06 18:13:16 -0300

    resolvendo parte da issue #19
    resolvendo parte da issue #19

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ require_once 'vendor/autoload.php';
 // endereço do site
 const URL = "http://localhost/ProjetoTurmaB-Consessionaria";
 $roteador = new CoffeeCode\Router\Router(URL);
-$roteador -> namespace("Concessionaria\Projetob\Controller");
+$roteador->namespace("Concessionaria\Projetob\Controller");
 
 // rota principal
-$roteador -> group(null);
-$roteador -> get("/", "Principal:inicio");
+$roteador->group(null);
+$roteador->get("/", "Principal:inicio");
 
-$roteador -> dispatch();
+// rota para detalhes do veículo
+$roteador->get("/veiculos/{id}", "VeiculosController:detalhes");
+
+$roteador->dispatch();
